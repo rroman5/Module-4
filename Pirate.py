29ecddabc3f99947dadb10b9901356739fbbb522
Roberto Roman <91975410+rroman5@users.noreply.github.com>
2021-10-25 12:28:36 -0500

    Module 4
    Programs assigned to debug

---
+++ Pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?") #<---  ""
+if greeting in ["Arrr!"]:   #<--- added [] to allign with each other
+	print("Go away, pirate.")
+else:   #<----- from  elif to else
+	print ("Greetings, hater of pirates!")
---
+++ time.py
@@ -0,0 +1,8 @@
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait")
+
+currentTimeInt = int(currentTimeStr)  #<---- ____ Not necessary also letters are not the same as above
+waitTimeInt = int(waitTimeStr)  #____ Not necessary also letters are not the same as above
+
+finalTimeInt = (currentTimeInt + waitTimeInt)
+print (finalTimeInt)
