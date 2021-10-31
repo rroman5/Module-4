eb8d6fcec157b6fbc6ae6cd38142373880d2f9e4
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-31 00:20:58 -0500

    Update time.py

--- time.py
+++ time.py
@@ -4,5 +4,9 @@ waitTimeStr = input("How many hours do you want to wait")
 currentTimeInt = int(currentTimeStr)  #<---- ____ Not necessary also letters are not the same as above
 waitTimeInt = int(waitTimeStr)  #____ Not necessary also letters are not the same as above
 
+'''
+Logic error
+finalTimeInt = (currentTimeInt + waitTimeInt) %24
+'''
 finalTimeInt = (currentTimeInt + waitTimeInt)
 print (finalTimeInt)
